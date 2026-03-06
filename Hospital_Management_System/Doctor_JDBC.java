6c449a7773431a03c0d93769b806fde1bd4373fb
Rafeeahmed09 <abdulrafee0078@gmail.com>
2026-03-06 22:10:34 +0530

    new code

---
+++ Hospital_Management_System/Doctor_JDBC.java
@@ -0,0 +1,13 @@
+package Hospital_Management_System;
+
+public class Doctor_JDBC {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/";
+    private static final String UER = "root";
+    private static final String Password = "1234567890";
+
+    // Added new Doctors
+    // View All
+    // Search
+    //
+}
