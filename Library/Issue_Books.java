4370fecd4a31b0a340bcbcc3e6bbac20786cb3cb
Rafeeahmed09 <abdulrafee0078@gmail.com>
2026-03-07 15:30:18 +0530

    new code

---
+++ Library/Issue_Books.java
@@ -0,0 +1,4 @@
+package Library;
+
+public class Issue_Books {
+}
---
+++ Medical_Store/Customers.java
@@ -0,0 +1,37 @@
+package Medical_Store;
+
+public class Customers {
+    private int id;
+    private String name;
+    private String phone;
+
+    public Customers(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
